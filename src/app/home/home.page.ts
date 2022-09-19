6702dff5b5cea9ee37a6115eecd64d39c7ca346f
unknown <philipmodino@gmail.com>
2022-09-19 22:40:10 +0200

    Add search backend, loading image

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -13,23 +13,26 @@ const API_KEY = environment.API_KEY;
 export class HomePage {
   weatherTemp :any
   todayDate = new Date();
-  cityName :any
+  cityName = ""
   weatherIcon:any
   weatherDetails:any
+  name=""
+  loading = true
 
   constructor(public httpClient:HttpClient) {
-    this.loadData();
+    // this.loadData();
   }
 
   loadData(){
-    this.httpClient.get(`${API_URL}/weather?q=${"Berlin"}&appid=${API_KEY}`).subscribe(results =>{
+    this.httpClient.get(`${API_URL}/weather?q=${this.cityName}&appid=${API_KEY}`).subscribe(results =>{
       console.log(results);
       this.weatherTemp = results['main'];
-      this.cityName=results['name'];
+      this.name=results['name'];
       console.log(this.weatherTemp);
       this.weatherDetails = results['weather'][0]
       console.log(this.weatherDetails);
       this.weatherIcon = `http://openweathermap.org/img/wn/${this.weatherDetails.icon}@4x.png` 
+      this.loading = false
     });
     
   }
