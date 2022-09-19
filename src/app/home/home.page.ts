2b12453bd231c836b2bd9da63a244eb84bdb0ae2
unknown <philipmodino@gmail.com>
2022-09-19 20:04:10 +0200

    Add temp,date, city

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,14 +11,19 @@ const API_KEY = environment.API_KEY;
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  weatherTemp :any
+  todayDate = new Date();
+  cityName :any
   constructor(public httpClient:HttpClient) {
     this.loadData();
   }
 
   loadData(){
     this.httpClient.get(`${API_URL}/weather?q=${"Berlin"}&appid=${API_KEY}`).subscribe(results =>{
-      console.log(results)
+      console.log(results);
+      this.weatherTemp = results['main'];
+      this.cityName=results['name'];
+      console.log(this.weatherTemp);
     });
     
   }
